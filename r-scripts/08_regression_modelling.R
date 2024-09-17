2683a94b9d8a80421c77adc0cf0a06e75b69a314
Sanne-BP <s.b.puister@student.rug.nl>
2024-09-17 10:20:49 +0200

    added excercises

--- r-scripts/08_regression_modelling.R
+++ r-scripts/08_regression_modelling.R
@@ -127,7 +127,7 @@ anova(m4,m3)
 
 # explore the consequences of a log transformation of y values
 x<-c(0,1,2,3,4,5)
-y<-c(1,10,100,1000,10000,100000)
+y<-c(0,0,120,1500,8000,100000)
 dat<-data.frame(x,y)
 dat
 dat %>% ggplot(aes(x=x,y=y)) +
@@ -135,10 +135,12 @@ dat %>% ggplot(aes(x=x,y=y)) +
   geom_line(size=1.2)
 dat %>% ggplot(aes(x=x,y=log10(y))) +
   geom_point(shape=16,size=3) +
-  geom_line(size=1.2)
+  #geom_line(size=1.2)
+  geom_smooth(method="lm")
 dat %>% ggplot(aes(x=x,y=log(y))) + # use log with base number e
   geom_point(shape=16,size=3) +
-  geom_line(size=1.2)
+  #geom_line(size=1.2)
+  geom_smooth(method="lm")
 # explore the consequences of the log base number (10 or e)
 log(0)
 log10(1)
@@ -153,21 +155,32 @@ log(exp(1))
 # Explore  how the abundance of Orchestia depends on elevation_m and year,  their potential interaction,
 # and a potential ecological optimum of Orchestia with respect to elevation_m
 # show the effect of elevation but now in a generalized linear model instead of linear model, using a log link function and a poisson distribution
-
+orchdat3
+p1
 
 #add the linear model to the plot
 # calculate the predicted value of m2 for every observation, add to the dataset as a variable as pred2
 # add the new predicted line to the previous plot p2, store as object p3 and show it
-
-
+m5 <- glm(CountSum~elevation_m, 
+          family = poisson(log), 
+          data=orchdat3)
 
 # now test and show  the effect of both elevation , elevation squared and year
-
+anova(m5, test = "Chisq") #significant, use Chi-square test!!!
+orchdat3$pred5 <- predict(m5, type="response")
+p1 + geom_line(data=orchdat3, aes(y=pred5), linewidth=1.2)
 
 #add the linear model to the plot
 # calculate the predicted value of m2 for every observation, add to the dataset as a variable as pred2
 # add the new predicted line to the previous plot p2, store as object p3 and show it
 
+#account for the ecological optimum by adding a quadratic term for elevation_m
+m6 <- glm(CountSum~elevation_m +I(elevation_m^2), 
+          family = poisson(log), 
+          data=orchdat3)
+
+orchdat3$pred6 <- predict(m6, type="response")
+p1 + geom_line(data=orchdat3, aes(y=pred6), linewidth=1.2)
 
 # better than the previous?
 
