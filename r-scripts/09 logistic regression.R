f93529ca0043c0c437b09fd5a761a0bd93c6c991
Sanne-BP <s.b.puister@student.rug.nl>
2024-09-17 11:31:20 +0200

    added excercises

--- r-scripts/09 logistic regression.R
+++ r-scripts/09 logistic regression.R
@@ -25,7 +25,8 @@ dat %>%
 # convert the data to long format and filter for 2023, exclude Salicornia.sp
 # sort by species and distance_m and 
 dat1<-dat %>% 
-  tidyr::pivot_longer(-c(year,Point_ID,x_coord,y_coord,elevation_m,claydepth_cm),
+  tidyr::pivot_longer(-c(year,Point_ID,x_coord,y_coord,elevation_m,
+                         claydepth_cm, distance_rtk_m),
                       names_to="species",
                       values_to = "presence") %>%
   dplyr::arrange(year,species,Point_ID) %>%
@@ -41,22 +42,56 @@ dat2<-dat1 %>%
                       names_to="soil_layer",
                       values_to = "layer_thickness_cm") 
 dat2
-
+p1 <- ggplot(data=dat2, aes(x=Point_ID, y=layer_thickness_cm, fill=soil_layer)) +
+               geom_area() +
+  coord_cartesian(ylim=c(95, 125)) #zooming into the graph without removing data
+p1
 
 # calculate the frequency (as a proportion) of occurrence of each species 
 # in 2023, and sort according to frequency
+dat3 <- dat1 |>
+  dplyr::filter(presence==1) |>
+  mutate(speciesnum=-as.numeric(factor(species)))
+p2 <- ggplot(data = dat3, aes(x=Point_ID, y=speciesnum, col=species))+
+  geom_point(size=2)
+p2
+
+p2 + p1 + patchwork::plot_layout(ncol = 1)
 
 ####### select Limonium vulgare and analyze and plot its response to elevation
-
+specsel <- "Spergularia.marina"
+p3 <- dat1 |> dplyr::filter(species==specsel) |>
+  ggplot(aes(x=elevation_m, y=presence)) +
+  geom_point(shape= "|") +
+  ggtitle(specsel)
+p3
 
 # calculate a logistic regression (also test quadratic term)
 # this is a generalized linear model with logit link and bionomial distribtion
-
+m1 <- dat1 |> dplyr::filter(species==specsel) |>
+  glm(presence~elevation_m, family=binomial(link=logit), data=_)
 
 # save the predicted values of this model (probability of occurrence) as a variable
-
+dat1 <- dat1 |>
+  dplyr::mutate(predicted=ifelse(species==specsel, 
+                                 predict(m1, type="response"), NA))
 
 # add the predicted values of the model to the graph as a line
+p3 + geom_line(data = dat1 |> dplyr::filter(!is.na(predicted)),
+               aes(y=predicted, col=species), linewidth=1.3)
+
+#test for quadratic effect
+m2 <- dat1 |> dplyr::filter(species==specsel) |>
+  glm(presence~elevation_m + I(elevation_m^2), family=binomial(link=logit), data=_)
+
+dat1 <- dat1 |>
+  dplyr::mutate(predicted=ifelse(species==specsel, 
+                                 predict(m2, type="response"), NA))
+
+anova(m2, m1, test = "Chisq")
+p3 + geom_line(data = dat1 |> dplyr::filter(!is.na(predicted)),
+               aes(y=predicted, col=species), linewidth=1.3)
+
 
 
 #### analyse the occurrence of Spergularia.marina in the same way
